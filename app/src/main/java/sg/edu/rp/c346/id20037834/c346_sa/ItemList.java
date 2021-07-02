463f08237b156694cd04bcc447859626acfab4a9
20037834 <20037834@myrp.edu.sg>
2021-07-02 16:39:08 +0800

    Added a item array list

--- app/src/main/java/sg/edu/rp/c346/id20037834/c346_sa/ItemList.java
+++ app/src/main/java/sg/edu/rp/c346/id20037834/c346_sa/ItemList.java
@@ -3,12 +3,31 @@ package sg.edu.rp.c346.id20037834.c346_sa;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class ItemList extends AppCompatActivity {
+
+    ArrayList<String> itemExpireArray;
+    ListView lvItemExpire;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_item_list);
+
+        lvItemExpire = findViewById(R.id.lvItemExpire);
+        itemExpireArray = new ArrayList<String>();
+
+        itemExpireArray.add("Expires 2021-8-2 Frozen Vegetables");
+        itemExpireArray.add("Expires 2021-8-6 Chicken");
+        itemExpireArray.add("Expires 2021-10-18 Frozen Pizza");
+        itemExpireArray.add("Expires 2021-10-20 Frozen Salmon");
+        itemExpireArray.add("Expires 2022-1-2 Frozen Meat");
+
+        ArrayAdapter adapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,itemExpireArray);
+        lvItemExpire.setAdapter(adapter);
     }
 }
