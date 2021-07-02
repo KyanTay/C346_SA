b07d4db6673b51d61db1bb77cd5611ebdfbedc9d
20037834 <20037834@myrp.edu.sg>
2021-07-02 17:53:17 +0800

    added date picker and add function

--- app/src/main/java/sg/edu/rp/c346/id20037834/c346_sa/ItemList.java
+++ app/src/main/java/sg/edu/rp/c346/id20037834/c346_sa/ItemList.java
@@ -2,8 +2,12 @@ package sg.edu.rp.c346.id20037834.c346_sa;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -11,6 +15,8 @@ import java.util.ArrayList;
 public class ItemList extends AppCompatActivity {
 
     ArrayList<String> itemExpireArray;
+    EditText etProductName;
+    Button btnAdd;
     ListView lvItemExpire;
 
     @Override
@@ -18,6 +24,8 @@ public class ItemList extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_item_list);
 
+        btnAdd = findViewById(R.id.btnAdd);
+        etProductName = findViewById(R.id.etProductName);
         lvItemExpire = findViewById(R.id.lvItemExpire);
         itemExpireArray = new ArrayList<String>();
 
@@ -27,7 +35,21 @@ public class ItemList extends AppCompatActivity {
         itemExpireArray.add("Expires 2021-10-20 Frozen Salmon");
         itemExpireArray.add("Expires 2022-1-2 Frozen Meat");
 
-        ArrayAdapter adapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,itemExpireArray);
-        lvItemExpire.setAdapter(adapter);
+        ArrayAdapter adapterProductList = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,itemExpireArray);
+        lvItemExpire.setAdapter(adapterProductList);
+
+
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String productName = etProductName.getText().toString();
+                itemExpireArray.add(productName);
+                etProductName.setText("");
+                etProductName.setHint("Enter Product Name");
+                adapterProductList.notifyDataSetChanged();
+
+            }
+        });
     }
 }
