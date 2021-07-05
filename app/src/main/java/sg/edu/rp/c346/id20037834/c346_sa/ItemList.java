7ae8b75dc2afb5f748d59c6277d61852a85b4c65
20037834 <20037834@myrp.edu.sg>
2021-07-05 18:19:44 +0800

    added filter and sorting methods

--- app/src/main/java/sg/edu/rp/c346/id20037834/c346_sa/ItemList.java
+++ app/src/main/java/sg/edu/rp/c346/id20037834/c346_sa/ItemList.java
@@ -4,13 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.TextView;
 
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Date;
 
 public class ItemList extends AppCompatActivity {
 
@@ -18,6 +28,8 @@ public class ItemList extends AppCompatActivity {
     EditText etProductName;
     Button btnAdd;
     ListView lvItemExpire;
+    Spinner monthPicker;
+    ArrayAdapter adapterProductList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,17 +40,18 @@ public class ItemList extends AppCompatActivity {
         etProductName = findViewById(R.id.etProductName);
         lvItemExpire = findViewById(R.id.lvItemExpire);
         itemExpireArray = new ArrayList<String>();
+        monthPicker = findViewById(R.id.spnMonthPicker);
 
-        itemExpireArray.add("Expires 2021-8-2 Frozen Vegetables");
+
+        itemExpireArray.add("Expires 2021-8-2 Vegetables");
         itemExpireArray.add("Expires 2021-8-6 Chicken");
-        itemExpireArray.add("Expires 2021-10-18 Frozen Pizza");
-        itemExpireArray.add("Expires 2021-10-20 Frozen Salmon");
-        itemExpireArray.add("Expires 2022-1-2 Frozen Meat");
+        itemExpireArray.add("Expires 2021-10-18 Pizza");
+        itemExpireArray.add("Expires 2021-10-20 Salmon");
+        itemExpireArray.add("Expires 2022-1-2 Meat");
 
-        ArrayAdapter adapterProductList = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,itemExpireArray);
+        adapterProductList = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,itemExpireArray);
         lvItemExpire.setAdapter(adapterProductList);
-
-
+        sortList();
 
         btnAdd.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -47,9 +60,88 @@ public class ItemList extends AppCompatActivity {
                 itemExpireArray.add(productName);
                 etProductName.setText("");
                 etProductName.setHint("Enter Product Name");
+                sortList();
                 adapterProductList.notifyDataSetChanged();
+            }
+        });
+
+        monthPicker.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                switch(position){
+                    case 0:
+                        dateFilter(1);
+                        break;
+                    case 1:
+                        dateFilter(3);
+                        break;
+                    case 2:
+                        dateFilter(6);
+                        break;
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
 
             }
         });
+    }
+
+    public void sortList(){
+
+        ArrayList<String> testing = new ArrayList<>();
+        //Reorder the text so product is infront of Date & store the new format in the temporary array.
+        for (int i = 0; i < itemExpireArray.size(); i++) {
+            String product = itemExpireArray.get(i).substring(itemExpireArray.get(i).indexOf(" ",10)+1, itemExpireArray.get(i).length());
+
+            String expireDate = " "+ itemExpireArray.get(i).substring(0, itemExpireArray.get(i).indexOf(" ",10));
+            testing.add(product + expireDate);
+        }
+
+        //Sort temporary array and clear the original to set in the sorted array
+        Collections.sort(testing);
+        itemExpireArray.clear();
+
+        //Re-order the temporary array so that it is back to the original format of expiry date first then product
+        //Then add it back to original array
+        for (int i = 0; i < testing.size(); i++) {
+
+            String product = testing.get(i).substring(0, testing.get(i).indexOf(" "));
+            String expireDate = testing.get(i).substring(testing.get(i).indexOf(" ")+1, testing.get(i).length());
+            itemExpireArray.add(expireDate +" "+ product);
+        }
+    }
+
+    public void dateFilter(int xMonth){
+        Date _sortingDate = new Date(new Date().getTime()+28800000);
+        ArrayList<String> filteredDateResult = new ArrayList<String>();
+        double oneMonth = 2629746000f;
+
+        for (int i = 0; i < itemExpireArray.size(); i++) {
+            //Get ONLY the expiry date as YYYY/MM/DD Format
+            String productDate = itemExpireArray.get(i).substring(itemExpireArray.get(i).indexOf(" ")+1, itemExpireArray.get(i).indexOf(" ", 10)).replaceAll("-","/");
+
+            //This is to intialise the variable
+            Date _productDate = null;
+            //This is to try to catch any possible error when trying to parse a data.
+            try {
+                _productDate = new SimpleDateFormat("yyyy/MM/dd").parse(productDate); //Convert the date in string to date dataType
+            }
+            //This is to catch the possible error.
+            catch (Exception e) {
+                //The parsing as date got error.
+            }
+
+            //Filtering
+            //If the expiry date is after the input's date
+            if (_productDate.getTime() > _sortingDate.getTime() && _productDate.getTime() < _sortingDate.getTime()+(oneMonth*xMonth)) {
+                filteredDateResult.add(itemExpireArray.get(i));
+            }
+        }
+        //This is to show only the filtered list
+        adapterProductList = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,filteredDateResult);
+        lvItemExpire.setAdapter(adapterProductList);
+        adapterProductList.notifyDataSetChanged();
     }
 }
